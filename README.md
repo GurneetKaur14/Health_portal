53401db77fdd999f73e9befd24768ff35f34784b
GurneetKaur14 <gurneetk2006@gmail.com>
2025-09-29 22:39:36 -0400

    Server.js created

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Health_portal
 This full stack project is a healthcare tracker and appointment portal for tracking health records and appointments.
+
+# Key Features:
+•	Authentication & Authorisation: The system will validate the user information before they are allowed to log in. Patients can only view their own data, while Doctors can see all the appointments and assigned patients.
+
+•	Health Tracking: To manage (create, update, delete, view) and set and monitor health goals, CRUD operations will be performed. Filtering data by date and paginating large datasets (e.g., daily logs over months/years)
+
+•	Appointment Manager: It will allow patients to browse doctors based on specialization, availability and location. Doctors can view the appointments requested and have the authority to confirm or reject an appointment. Pagination on a large appointment list.
+
+•	Records and History: Doctors can add prescriptions, diagnoses or leave any required comments. Patients can view their medical history.
+
+•	Progress Dashboard: Graphs and charts for steps, calories, and heart rate trends. 
--- server.js
+++ server.js
@@ -2,28 +2,62 @@
 const express = require("express");
 const app = express();
 
+//Health Tracker routes
 app.get("/health_tracker", (req, res ) => {
-    res.send("Set and track your health goals");
+    res.send("Track your health goals");
 });
 
+app.post("/health_tracker", (req, res) => {
+    res.send("Add a new health goal")
+});
+
+app.put("/health_tracker/:id", (req, res) => {
+    res.send(`Update health goal with ID ${req.params.id}`);
+});
+
+app.delete("/health_tracker/:id", (req, res) => {
+    res.send(`Delete health goal with ID ${req.params.id}`);
+});
+
+//Appointment manager routes
 app.get("/appointment_manager", (req, res) => {
     res.send("Book and manage your appointments here.")
 });
 
-app.get("/login", (req,res, next) => {
+app.post("/appointments", (req, res) => {
+    res.send("Book a new appointment");
+});
+
+app.delete("/appointments/:id", (req, res) => {
+    res.send(`Cancel appointment with ID ${req.params.id}`);
+});
+
+//Login Route
+app.get("/login", (req,res) => {
     res.send("Login")
 });
 
-app.get("/health_records", (req, res, next) => {
+//Health records routes
+app.get("/health_records", (req, res) => {
     res.send("Records and History")
 });
 
-app.get("/health_metrics", (req, res, next) => {
+app.get("/health_records/:id", (req, res) => {
+    res.send(`Get record with ID ${req.params.id}`);
+});
+
+app.post("/health_records", (req, res) => {
+    res.send("Add a new health record");
+});
+
+//Health metrics routes
+app.get("/health_metrics", (req, res) => {
     res.send("Progress Dashboard")
 });
 
+//Home Route
 app.get("/", (req, res) => {
-    res.send("Welcome to the Health Portal 🚀");
+    res.send("Welcome to the Health Portal");
 });
 
 app.listen(3000, () => {
